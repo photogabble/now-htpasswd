c71da76e9dd83b31753104bb06a98659692d5dad
Simon Dann <carbontwelve@users.noreply.github.com>
2019-08-21 13:36:55 +0100

    Working .htpasswd example (#2)
    * Create index.html
    
    * Adding 404 response
    
    * Added missing os import + 404 if request is for a directory
    
    * Adding protected files as included for lambada
    
    * first attempt at basic auth
    
    * Add X-Authenticated-Username header
    
    * added readme stub
    
    * Create LICENSE
    
    * Add go report card
    
    * Use tg123/go-htpasswd
    
    * Adding example .htpasswd file
    
    * Include .htpasswd
    
    * Forgot error handler for .htpasswd loading
    
    * Update README.md

--- default.go
+++ default.go
@@ -1,14 +1,81 @@
 package handler
 
 import (
-	"fmt"
+	"os"
 	"net/http"
-	"time"
+	"path/filepath"
+	"strings"
+	"encoding/base64"
+	"github.com/tg123/go-htpasswd"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format(time.RFC850)
+var auth *htpasswd.File
+var authLoadErr error
+
+func authenticate(w http.ResponseWriter, r *http.Request) bool {
+	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(s) != 2 {
+		return false
+	}
+
+	b, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil {
+		return false
+	}
 	
-	fmt.Fprintf(w, r.URL.Path)
-	fmt.Fprintf(w, currentTime)
+	pair := strings.SplitN(string(b), ":", 2)
+	if len(pair) != 2 {
+		return false
+	}
+	
+	ok := auth.Match(pair[0], pair[1])
+	
+	if ok {
+		w.Header().Set("X-Authenticated-Username", pair[0])
+	}
+	
+	return ok
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {	
+	// Load .htpasswd file
+	auth, authLoadErr = htpasswd.New("./.htpasswd", htpasswd.DefaultSystems, nil)
+	
+	if authLoadErr != nil {
+		w.WriteHeader(500)
+        	w.Write([]byte("500 Internal Server Error\n"))
+		return
+	}
+	
+	// Build protected file path from http request
+	fp := filepath.Join("protected", filepath.Clean(r.URL.Path))
+	if fp == "protected" {
+		fp = filepath.Join(fp, "index")
+	}
+	fp = "./" + fp + ".html"
+		
+	// Return a 404 if the protected file does not exist
+	info, err := os.Stat(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.NotFound(w, r)
+			return
+		}
+	}
+	
+	// Return a 404 if the request is for a directory
+	if info.IsDir() {
+		http.NotFound(w, r)
+		return
+	}
+	
+	// Check if user is authenticated
+	if authenticate(w,r) {
+		http.ServeFile(w,r,fp)
+		return
+	}
+	
+	w.Header().Set("WWW-Authenticate", `Basic realm="Beware! Protected REALM! "`)
+        w.WriteHeader(401)
+        w.Write([]byte("401 Unauthorized\n"))
 }
