cf416fdb4269661052ee05c2798de56b6e424579
Simon Dann <carbontwelve@users.noreply.github.com>
2019-08-21 11:00:52 +0100

    Update default.go

--- default.go
+++ default.go
@@ -8,5 +8,12 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fp := filepath.Join("protected", filepath.Clean(r.URL.Path))
+	
+	if fp == "protected" {
+		fp = fp+"index"
+	}
+	
+	fp = fp + ".html"
+	
 	fmt.Fprintf(w, fp)
 }
